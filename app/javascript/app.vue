a9c187dfbbab0007ea8fe68a57e64ff970b66fbc
lucasrena <lucas.rena@gmail.com>
2019-12-07 19:23:48 -0300

    First vue component working

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <searchbox/>
     <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
+import Searchbox from './components/Searchbox.vue'
+
 export default {
+  components: {
+    Searchbox
+  },
   data: function () {
     return {
       message: "Hello Vue!"
---
+++ app/javascript/components/Searchbox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search-wrapper">
+    <input type="text" v-model="search" placeholder="Search by name..."/>
+    <label>Search company:</label>
+    <div>
+      <p>Message is: {{ search }}</p>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Searchbox',
+  data () {
+    return {
+      search: ''
+    }
+  },
+  created () {
+    //this.checkSignedIn()
+  },
+  updated () {
+    //this.checkSignedIn()
+  },
+  methods: {
+    searchCompanies () {
+    //   this.$http.plain.post('/signin', { email: this.email, password: this.password })
+    //     .then(response => this.signinSuccessful(response))
+    //     .catch(error => this.signinFailed(error))
+    }
+  }
+}
+</script>
